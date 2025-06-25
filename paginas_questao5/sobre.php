418aa74ba066f5cca44f97bfc123617eca870317
nathy-melo <natymelo2008@gmail.com>
2025-06-25 10:10:55 -0300

    nt

--- paginas_questao5/sobre.php
+++ paginas_questao5/sobre.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sobre</title>
-</head>
-<body>
-    <h1>PÁGINA SOBRE</h1>
-</body>
-</html>
+<h1>PÁGINA SOBRE</h1>
